69f7beefcd1d9f7df1d833432a34e4a50aa8a3a1
Fatih <nzvdgzygrk@privaterelay.appleid.com>
2026-08-16 02:04:13 +0300

    Refresh Safari gate cache after role change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fk-sprintgate-gate-v2';
+const CACHE='fk-sprintgate-gate-v3';
 const ASSETS=['./','./index.html'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
